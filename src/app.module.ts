4cd60271ddd1ff203025ae1f8691c412a020968f
hoangdv2429 <hoangdv2429@gmail.com>
2022-08-08 22:15:22 +0700

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FoodsModule } from './foods/foods.module';
 
 @Module({
-  imports: [FoodsModule],
+  imports: [
+    FoodsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/foodtown-mongo'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
