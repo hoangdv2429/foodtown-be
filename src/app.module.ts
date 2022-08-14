f44c526bb30569187d39feb69599741c32e29458
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-08-15 00:41:25 +0700

    add auth & user & post module ** still have bug on fine one post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,45 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import PostsModule from './posts/posts.module';
+import * as Joi from '@hapi/joi';
+import { AuthenticationModule } from './authentication/authentication.module';
+import CategoriesModule from './categories/categories.module';
+import SeriesModule from './series/series.module';
 import { FoodsModule } from './foods/foods.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_USERNAME: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_DATABASE: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+
+        return {
+          uri: `mongodb://${username}:${password}@${host}`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    PostsModule,
+    AuthenticationModule,
+    CategoriesModule,
+    SeriesModule,
     FoodsModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/foodtown-mongo'),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
