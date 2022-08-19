5141f093b5600bf5c414f6a9c2be163c067d72b1
hoangdv2429 <hoangdv2429@gmail.com>
2022-08-19 20:43:25 +0700

    fix length schema of phoneNumber

--- src/authentication/dto/register.dto.ts
+++ src/authentication/dto/register.dto.ts
@@ -5,6 +5,7 @@ import {
   MinLength,
   Length,
   Matches,
+  MaxLength,
 } from 'class-validator';
 
 export class RegisterDto {
@@ -26,7 +27,8 @@ export class RegisterDto {
 
   @IsString()
   @IsNotEmpty()
-  @Length(10)
+  @MinLength(9)
+  @MaxLength(10)
   @Matches(/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/, {
     message: 'Invalid phone number format',
   })
