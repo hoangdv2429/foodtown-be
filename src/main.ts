3fb8b20b5e8d96c30697b0fe6c34cd2939b75000
hoangdv2429 <hoangdv2429@gmail.com>
2022-08-19 16:56:16 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
   await app.listen(3000);
