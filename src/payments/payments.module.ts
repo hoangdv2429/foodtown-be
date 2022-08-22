6fcb0622597757e97cd52d5c153070b1735802c2
hoangdv2429 <hoangdv2429@gmail.com>
2022-08-23 00:52:18 +0700

    allow user to register as admin (temporarily) && update to payment module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,11 +4,13 @@ import { PaymentsController } from './payments.controller';
 import { CartModule } from 'src/cart/cart.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Payment, PaymentSchema } from './payments.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
     CartModule,
+    UsersModule,
   ],
   providers: [PaymentsService],
   controllers: [PaymentsController],
